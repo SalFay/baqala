550d95c8fcf3944164a1461c2a5e2c6eb4c1be32
Haroon Yousaf <haroonubl0177@hotmail.com>
2023-07-08 15:45:17 +0500

    config changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>{{ config('app.name', 'Laravel') }}</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link rel="preconnect" href="https://fonts.bunny.net">
 	<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-	<!-- Scripts -->
-	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 	@livewireStyles
 	@stack('head')
 </head>
