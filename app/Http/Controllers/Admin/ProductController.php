8a364d3af6e66718c2ede6d0824dcfa729299303
Haroon Yousaf <hyousaf@uetpeshawar.edu.pk>
2025-01-12 10:47:22 +0500

    changes

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -12,37 +12,38 @@ use Yajra\DataTables\DataTables;
 
 class ProductController extends Controller
 {
-  
-  private $service;
-  
-  public function __construct( ProductService $service )
-  {
-    $this->service = $service;
-  }
-  
-  public function index( ProductsDataTable $dataTables )
-  {
-    return $dataTables->render( 'admin.products.index' );
-  }
-  
-  public function print( Request $request )
-  {
-    $products = Product::where( 'status', 'Active' )->get();
-    return view( 'admin.products.barcode', compact( 'products' ) );
-  }
-  
-  public function sticker( Product $product )
-  {
-    return view( 'admin.products.sticker-barcode', compact( 'product' ) );
-  }
-  
-  /**
-   * @param Product $product
-   * @return JsonResponse
-   */
-  public function edit( Product $product ) : JsonResponse
-  {
-    return new JsonResponse( $product );
-  }// edit
-  
+
+    private $service;
+
+    public function __construct(ProductService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(ProductsDataTable $dataTables)
+    {
+        return $dataTables->render('admin.products.index');
+    }
+
+    public function print(Request $request)
+    {
+        $products = Product::where('status', 'Active')->get();
+        return view('admin.products.barcode', compact('products'));
+    }
+
+    public function sticker(Product $product)
+    {
+        return view('admin.products.sticker-barcode', compact('product'));
+    }
+
+    /**
+     * @param Product $product
+     * @return JsonResponse
+     */
+    public function edit(Product $product): JsonResponse
+    {
+        dd($product);
+        return new JsonResponse($product);
+    }// edit
+
 }
